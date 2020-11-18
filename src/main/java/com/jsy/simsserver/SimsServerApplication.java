db33bf2c1ae6313244de87bcfc8bd343bd40ea81
SongyangJi <1539242799@qq.com>
2020-11-18 22:10:47 +0800

    Initial commit

---
+++ src/main/java/com/jsy/simsserver/SimsServerApplication.java
@@ -0,0 +1,13 @@
+package com.jsy.simsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimsServerApplication.class, args);
+    }
+
+}
